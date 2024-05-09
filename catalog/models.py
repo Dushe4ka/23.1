451ae4cfe83cbefdf6d4e4715c4e9b266ed5a679
Pavel Golubinec <7respawnables7@gmail.com>
2024-05-09 11:03:09 +0300

    20.1 09.05.2024 migrate 1

--- catalog/models.py
+++ catalog/models.py
@@ -14,32 +14,69 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Наименование', help_text='Введите название категории')
-    description = models.TextField(verbose_name='Описание', help_text='Введите описание')
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Наименование",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание"
+    )
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Продукт', help_text='Введите наименование')
-    description = models.TextField(verbose_name='Описание', help_text='Введите описание')
-    image = models.ImageField(upload_to='product/photo', verbose_name='Изображение', help_text='Загрузите изображение')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', help_text='Выберите категорию')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку', help_text='Введите цену')
-    created_at = models.DateTimeField(blank=True, null=True, verbose_name='Дата создания', help_text='Укажите дату создания')
-    updated_at = models.DateTimeField(blank=True, null=True, verbose_name='Дата последнего изменения', help_text='Укажите дату последнего изменения')
+    name = models.CharField(
+        max_length=100, verbose_name="Продукт", help_text="Введите наименование"
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание"
+    )
+    image = models.ImageField(
+        upload_to="product/photo",
+        verbose_name="Изображение",
+        help_text="Загрузите изображение",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Выберите категорию",
+        related_name="products",
+        null=True,
+        blank=True,
+        related_query_name="product",
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена за покупку",
+        help_text="Введите цену",
+    )
+    created_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+    )
+    updated_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения",
+        help_text="Укажите дату последнего изменения",
+    )
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name', 'category', 'price', 'created_at']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "category", "price", "created_at"]
 
     def __str__(self):
         return self.name, self.category, self.price, self.created_at
-

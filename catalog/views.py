8fd9b28153ed8a09832c4f083bfe8de5455f5d26
Pavel Golubinec <7respawnables7@gmail.com>
2024-05-15 17:59:48 +0300

    15.05.2024

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
 def contacts(request):
@@ -18,7 +18,14 @@ def contacts(request):
 
     return render(request, 'contacts.html')
 
-# def products_list(request):
-#     products = Product.objects.all()
-#     context = {"products": products}
-#     return render(request, 'err.html', context)
+
+def home(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'Product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'Product_detail.html', context)
